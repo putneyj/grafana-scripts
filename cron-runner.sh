c6fdf0fe69ebf0ded1536d01075d482c94fa8932
Jonathan Putney <jonathan.putney@me.com>
2018-11-20 21:14:38 -0500

    Using current directory of script

--- cron-runner.sh
+++ cron-runner.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && DIR )"
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
 
 command python3 $DIR/sonarr.py --missing --upcoming --queue
 command python3 $DIR/radarr.py --missing_avl --queue
