99f6859c6abb1a4b2b831e5da4085085346bcb31
Jonathan Putney <jonathan.putney@me.com>
2018-11-20 21:01:25 -0500

    Typo in runner

--- cron-runner.sh
+++ cron-runner.sh
@@ -3,4 +3,4 @@
 command python3 $PWD/sonarr.py --missing --upcoming --queue
 command python3 $PWD/radarr.py --missing_avl --queue
 command python3 $PWD/ombi.py --total --counts
-command python3 $PWD/tautalli.py
+command python3 $PWD/tautulli.py
